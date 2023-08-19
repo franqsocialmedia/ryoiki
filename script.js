37a0242e58ddc71cf7c922698893e0d608c3ca09
Yan-Yan <franqsocialmedia@gmail.com>
2023-08-19 03:09:10 -0400

    antes de la prueba definitiva

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//alert ("HOLA GIANT");
+
+
+
+var turno = document.getElementById('jugador').value;
+var g = document.getElementById('prueba');
+
+function x_o(x){
+	if(turno==1){
+g.value = turno;
+	}else if(x=="a2"){
+g.innerHTML = turno;
+	}else if(x=="a3"){
+g.innerHTML = turno;
+	
+	}else if(x=="b1"){
+g.innerHTML = turno;
+	}else if(x=="b2"){
+g.innerHTML = turno;
+	}else if(x=="b3"){
+g.innerHTML = turno;
+	
+	}else if(x=="c1"){
+g.innerHTML = turno;
+	}else if(x=="c2"){
+g.innerHTML = turno;
+	}else if(x=="c3"){
+g.innerHTML = turno;
+		}
+}
