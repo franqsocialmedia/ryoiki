72438732fdbe4c1e8b882ffde267d4f564fca5bb
Yan-Yan <franqsocialmedia@gmail.com>
2023-08-23 16:52:05 -0400

    version fallida de la pagina blog Jujutsukaisen porque le falta optimizacion. Esta version muestra las falencias de velocidad y errores de lectura de parte del navegador.Proxima elaboracion: 1) aprender a documentar un proceso de desarrollo web... 2) Hacer que las imagenes se expandan y muestren una version ampliada... 3) pulido de pagina con diferentes versiones de imagenes... 4) Tematica con diferentes secciones y con paginas internas... 5) JQUERY, BOOSTRAP y aprender lo basico de ECMACSCRIPT... 6) UX con boceto previo... todo eso para la siguiente pagina. Luego seria hacer otras 4 paginas usando todo ese conocimiento y haciendolo en tiempos records.

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,5 +1,6 @@
 var opciones = document.getElementById("menu");
 var cover = document.getElementById("cover");
+var perfil = document.querySelector(".perfil");
 
 function OpenCloseMenu(){
 	
@@ -14,4 +15,16 @@ function OpenCloseMenu(){
 	}else{
 		console.log("NO EXISTE");
 	}
+}
+
+function adios(){
+	console.log("Adios a la figura");
+	
+	perfil.classList.add("perfil_off");
+}
+
+function hola(){
+	console.log("Hola a la figura");
+	
+	perfil.classList.remove("perfil_off");
 }
