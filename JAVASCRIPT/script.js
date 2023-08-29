f0917fb56027d4ddbaf4dabc0e0f3b11a33399be
Yan-Yan <franqsocialmedia@gmail.com>
2023-08-28 23:04:51 -0400

    Ajustar el tema de los HOVER. 2) Agregar funcionalidades de play conteo. 3) Hacer el efecto de responsive

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,11 @@
 //alert("Hola Yan-Yan");
+
+const sec = new Date();
+
+document.getElementById("likes").innerHTML = sec.getSeconds();
+
+function menu(){
+	document.querySelector('.menu_pop').classList.toggle('open');
+	
+	console.log("leyo");
+}
