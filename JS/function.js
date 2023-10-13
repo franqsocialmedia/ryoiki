586684bd5a21cac726121cb51fcd389c831e8086
Yan-Yan <franqsocialmedia@gmail.com>
2023-10-12 21:58:04 -0400

    feat (chore): Structure done. Have to finish the Style and JS Functionalities

--- JS/function.js
+++ JS/function.js
@@ -4,3 +4,12 @@ function menuClick(){
 	var menu_activo = document.querySelector('.menu').classList.toggle('menu_activo');
 	var cover_activo = document.querySelector('.cover').classList.toggle('cover_activo');
 }
+
+
+function gallery_show(){
+	console.log("GALERIA");
+	
+	var img = document.getElementById('imagen_galeria').url;
+	
+	alert(img);
+}
