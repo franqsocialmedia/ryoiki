4194d5cb3b5dfe1e4749cfc42e73432f69b62538
Yan-Yan <franqsocialmedia@gmail.com>
2023-10-24 22:20:19 -0400

    feat (chore): fix the Portrait version style and try the dragging of the images and element modify

--- JS/function.js
+++ JS/function.js
@@ -6,8 +6,43 @@ function menuClick(){
 }
 
 var x = document.getElementById('galeria');
+var IMG_muestra = document.getElementById('none');
+var IMG_fondo = document.getElementById('fondo');
 
-x.addEventListener ('click', (e) => {
-	console.log(x.target);
-})
+let agregar = document.getElementById('galeria');
 
+var viendo = 0;
+
+function muestraIMG() {
+
+	if(viendo == 0){
+	let new_img = document.createElement('img');
+
+	var IMG_link = event.target.src;
+	console.log(IMG_link);
+	
+	IMG_muestra.classList.toggle("NOviendo");
+	IMG_fondo.classList.toggle("NOviendo");
+	IMG_muestra.src = IMG_link;
+	
+	console.log("ESTO ES: " + IMG_muestra.src);
+	
+	new_img.classList.add("galeria_img");
+	new_img.classList.add("NOviendo");
+	
+	new_img.src = "IMG/no-found.png";
+	new_img.onclick = "muestraIMG()";
+	new_img.id = "imagen_galeria";
+	
+	agregar.appendChild(new_img);
+
+	viendo = 1;
+	}else{
+		
+		IMG_muestra.classList.toggle("NOviendo");
+		IMG_fondo.classList.toggle("NOviendo");
+	
+		
+		viendo = 0;
+	}
+}
