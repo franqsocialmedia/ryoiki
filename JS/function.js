d90e33d94faa97dc596ac4e396fdc6db591e25b8
Yan-Yan <franqsocialmedia@gmail.com>
2023-10-27 23:47:26 -0400

    feat (build): Finish the proyect with the style of the app

--- JS/function.js
+++ JS/function.js
@@ -4,17 +4,33 @@ var espacioLista = document.getElementById('lista');
 var btnAgregar = document.getElementById('btn_agregar');
 
 function newTask(){
-	let task = prompt("agrege nueva tarea");
-	//const nuevaTarea = '<li class="list_item"><input type="checkbox" class="check_seleccion"><div><span>"'+task+'"</span><i>30 AGO - 17:00</i></div></li>';
-	/*
-	const taskCreate = document.createElement("li");	
-	const taskAdd = taskCreate.innerHTML = nuevaTarea;	
-	const espacioAdd = document.espacioLista.appendChild(taskCreate);
-	*/
+	//let task = prompt("agrege nueva tarea");
+	
+	var screen = document.querySelector('.x');
+	screen.classList.toggle('activo');
+	
+	document.querySelector('.tarea').focus;
+}
+	
+	
+function registrarTask(){
+	
+	var screen = document.querySelector('.x');
+	var task = document.querySelector('.tarea').value;
+	screen.classList.toggle('activo');
+	
+	
+	if (task == ""){
+		alert("ESTA VACIO");
+		
+		console.log("CAMPO VACIO");
+	}else{
+	
+	
 	
 	//LISTA
 	const nuevaLista = document.createElement("li");
-	
+	nuevaLista.className = "list_item";
 	//INPUT
 	const nuevoCheck = document.createElement("input");
 	nuevoCheck.type = "checkbox";
@@ -39,6 +55,49 @@ function newTask(){
 	nuevoDiv.appendChild(nuevoSpan);
 	nuevoDiv.appendChild(nuevoItalic);
 	
-	
 	console.log("agregando...");
+	
+	document.querySelector('.tarea').value = "";
+	}
 }
+
+document.body.addEventListener("click",eliminarTask);
+
+function eliminarTask(event){
+	console.log("ELIMINANDO...");
+	var x = event.target;
+	
+	if(x.classList == "list_item"){
+	
+	console.log("ELIMINANDO");
+	document.getElementById('lista').removeChild(x);
+	}else{
+	console.log("NO ELIMINANDO");
+	}
+	
+	}
+
+/*codigo posiblemente funcional para un juego de click
+
+//document.body.addEventListener("click", moverElemento);
+function moverElemento(event){
+	var objeto = event;
+	var idObjeto = objeto.target.id;
+	var nuevoLugar = document.getElementById(idObjeto);
+	
+	console.log("MOVIENDO:" + objeto.target.tagName);
+	console.log(objeto.pageX);
+	
+	var newPosition = document.getElementById('pruebaMove');
+
+	newPosition.style.top = objeto.pageY;
+	newPosition.style.bottom = objeto.pageY;
+	newPosition.style.left = objeto.pageX;
+	newPosition.style.right = objeto.pageX;
+	
+}
+
+function depositarElemento(event){
+	
+	console.log("DEPOSITO: " + event.target.tagName);
+}*/
