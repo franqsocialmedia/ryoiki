362499ef8141cc03c35f6d1d98577ff6e8d0d292
Yan-Yan <franqsocialmedia@gmail.com>
2023-11-07 22:40:59 -0400

    feat (chore): The main control buttons are done. Up, Down, Right, Left and Space button. The Volume and the main character, stage and the obstacule. The next step is the collision

--- JS/function.js
+++ JS/function.js
@@ -28,3 +28,75 @@ function bajar(){
 	
 	console.log(barra.style.width);
 	}
+	
+document.body.addEventListener("keydown", boton);
+	
+function boton(event){
+
+		var btnRight = document.getElementById('btn_4');
+		var btnLeft = document.getElementById('btn_1');
+		var btnDown = document.getElementById('btn_3');
+		var btnUp = document.getElementById('btn_2');
+		var btnEspacio = document.getElementById('btn_5');
+		
+		let value= event.which;
+		
+		//DERECHA
+		if(value == 97 || value == 68 || value == 39){
+		console.log('A => DERECHA ' + value);
+		
+		var btnRight = document.getElementById('btn_4');
+		
+		btnRight.style.background = 'red';
+		btnLeft.style.background = 'black';
+		btnUp.style.background = 'black';
+		btnDown.style.background = 'black';
+		btnEspacio.style.background = 'black';
+		
+		//IZQUIERDA
+		}else if(value == 100 || value == 65 || value == 37){
+		console.log('D => IZQUIERDA ' + value);
+		
+		btnLeft.style.background = 'red';
+		btnRight.style.background = 'black';
+		btnUp.style.background = 'black';
+		btnDown.style.background = 'black';
+		btnEspacio.style.background = 'black';
+		
+		//ABAJO
+		}else if(value == 115 || value == 83 || value == 40){
+		console.log('S => ABAJO ' + value);
+		
+		btnDown.style.background = 'red';
+		btnLeft.style.background = 'black';
+		btnUp.style.background = 'black';
+		btnRight.style.background = 'black';
+		btnEspacio.style.background = 'black';
+		
+		//ARRIBA
+		}else if(value == 119 || value == 87 || value == 38){
+		console.log('W => ARRIBA ' + value);
+		
+		btnUp.style.background = 'red';
+		btnLeft.style.background = 'black';
+		btnRight.style.background = 'black';
+		btnDown.style.background = 'black';
+		btnEspacio.style.background = 'black';
+		
+		//BARRA
+		}else if(value == 32){
+		console.log('ESPACIO => ESPACIO ' + value);
+		
+		
+		btnEspacio.style.background = 'red';
+		btnLeft.style.background = 'black';
+		btnRight.style.background = 'black';
+		btnDown.style.background = 'black';
+		btnUp.style.background = 'black';
+		
+		//PARA TODOS LOS DEMAS
+		}else{
+		
+		console.log(value);
+		}
+}
