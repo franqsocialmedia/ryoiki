43b480a716115a8bca25c8709664612e33d11c5a
Yan-Yan <franqsocialmedia@gmail.com>
2023-11-11 03:33:52 -0400

    feat (chore): Now I just need to create the collision

--- JS/function.js
+++ JS/function.js
@@ -3,17 +3,11 @@
 	var barra = document.getElementById('barra');
 	vid.volume = 0.1;
 	var porcentaje = vid.volume*100;
-	var protagonista = document.getElementById('personaje');
-	var obstaculo = document.getElementById('obstaculo');
-	
-	if(protaPosition){
-	
-	}else{
-	var protaPosition = protagonista.style.left = 0;
-	}
-	
 	barra.style.width = porcentaje;
+
 	
+	
+//Volumen	
 function subir(){
 
 	vid.volume = vid.volume + 0.1;
@@ -24,7 +18,7 @@ function subir(){
 	
 	console.log(barra.style.width);
 	}
-	
+
 function bajar(){
 
 	vid.volume = vid.volume - 0.1;
@@ -38,15 +32,112 @@ function bajar(){
 	}
 	
 document.body.addEventListener("keydown", boton);
+document.body.addEventListener("keyup", soltar);
 
-function avanzar(){
+var tu = 0;
+var pelota = 100;
 
-	console.log('avanzar '+ protagonista.style.position.left);
-	protagonista.style.position.left = protagonista.style.position.left + 700;
+//CANVAS
+var canvas = document.getElementById("canvas");
+var objeto = canvas.getContext('2d');
+
+const getCharacter = (color='blue') => ({
+	x:0,
+	y:0,
+	w:10,
+	h:30,
+	collision:this.y + this.h,
+	color,
+	
+	aparece(x,y) {
+		objeto.fillStyle = this.color
+		objeto.fillRect(x,y,this.w,this.h)
+	}
+});
+
+const getBall = (t=5,color='black') => ({
+	x:100,
+	y:100,
+	w:t,
+	h:t,
+	color,
+	direction:'r',
 	
 	
+	aparece(x) {
+	
+	if(this.direction == 'r'){
+		
+			this.x++;
+		if(this.x > canvas.width - prota.w){
+			this.direction = 'l';
+		}
+	}else{
+			this.x--;
+		if (this.x < prota.w && this.y == prota.y){
+			this.direction = 'r';
+		}
+	}
+		
+		objeto.fillStyle = this.color
+		objeto.fillRect(this.x,this.y,this.w,this.h)
+	}
+});
+
+const prota = getCharacter();
+const enemy = getCharacter('red');
+const ball = getBall();
+
+var x_Prota = 0;
+var y_Prota = 0;
+
+var x_Enemy = canvas.width - prota.w;
+var y_Enemy = 100;
+
+console.log('PROTAGONISTA: '+prota);
+console.log('ENEMIGO: '+enemy);
+
+const update = () => {
+	objeto.clearRect(0,0,canvas.width,canvas.height);
+	
+	if(tu == 0){
+	prota.aparece(x_Prota,y_Prota);
+	enemy.aparece(x_Enemy,y_Enemy);
+	
+	}else if(tu > 0){
+	prota.aparece(x_Prota,y_Prota++);
+	enemy.aparece(x_Enemy,y_Enemy);
+	
+	}else if(tu < 0){
+	prota.aparece(x_Prota,y_Prota--);
+	enemy.aparece(x_Enemy,y_Enemy);
+	}
+	
+	
+
+	
+	
+	if(ball.x < (canvas.x - prota.w)){
+		ball.aparece();
+	}else{
+		ball.aparece();
+	}
+	
+	
+console.log('POSITION: '+prota.y);
+console.log('TAMANO: '+ prota.h);
+	
+	requestAnimationFrame(update);
 }
 
+requestAnimationFrame(update);
+
+//movimientos
+function avanzar(){
+
+}
+
+//controles
 function boton(event){
 
 		var btnRight = document.getElementById('btn_4');
@@ -91,6 +182,8 @@ function boton(event){
 		btnRight.style.background = 'black';
 		btnEspacio.style.background = 'black';
 		
+		tu = 1;
+		
 		//ARRIBA
 		}else if(value == 119 || value == 87 || value == 38){
 		console.log('W => ARRIBA ' + value);
@@ -100,6 +193,8 @@ function boton(event){
 		btnRight.style.background = 'black';
 		btnDown.style.background = 'black';
 		btnEspacio.style.background = 'black';
+		
+		tu = -1;
 		
 		//BARRA
 		}else if(value == 32){
@@ -117,4 +212,8 @@ function boton(event){
 		
 		console.log(value);
 		}
+}
+
+function soltar(){
+tu = 0;
 }
