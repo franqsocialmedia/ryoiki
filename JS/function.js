d83b56c3f4471eeba4e5e0a2689880b6e0bf69bb
Yan-Yan <franqsocialmedia@gmail.com>
2023-11-18 23:32:05 -0400

    feat (build): The mini-platform game is done! Now the last one is a shuffle card game. And try to activate SFX with the actions.

--- JS/function.js
+++ JS/function.js
@@ -1,3 +1,21 @@
+
+var tiempo = 1000;
+var s = document.getElementById('segundos');
+
+function newTime(){
+
+const seg = new Date()
+segundos = seg.getSeconds();
+setTimeout(newTime, tiempo);
+
+s.innerHTML = segundos;
+//console.log(segundos);
+};
+newTime();
+
+sessionStorage.setItem('apareceFoe',segundos);
+var rango = sessionStorage.getItem('apareceFoe') + 5;
+
 //controles
 
 var btnRight = document.getElementById('btn_4');
@@ -84,10 +102,9 @@ console.log('PRECIONADO BOTON');
 		btnDown.style.background = 'white';
 		btnUp.style.background = 'white';
 		
-		if(tu.jump == 0){
-			tu.jump = 30;
-		}else if(tu.jump > 25){
-			tu.jump = 5;
+		if(tu.jump == 0 && tu.intento == 1){
+			tu.jump = 50;
+		}else{
 		}
 		
 		//PARA TODOS LOS DEMAS
@@ -108,12 +125,22 @@ function soltar(event){
 
 //CODIGO GAMEPLAY
 
+var barra = document.getElementById('barra');
+var header = document.getElementById('header');
 var canvas = document.getElementById('canvas');
+var gameOver = 0;
+
 
 canvas.style.width = '90%';
 canvas.style.height = '40%';
 
 const lugar = canvas.getContext('2d');
+
+class vida{
+	constructor(){
+	this.barra = 0;
+	}
+}
 
 class personaje{
 	constructor(x,y,w,h,color,gravedad,speed,imagen,life,salto){
@@ -125,27 +152,32 @@ class personaje{
 	this.gravedad = 1;
 	this.speed = 1;
 	this.imagen = 'URL';
-	this.life = 100;
+	this.life = canvas.width;
 	this.salto = 0;
+	this.intento = 1;
+	this.inmunidad = 1;
 	}
 }
 
 class foe{
 	constructor(x,y,w,h,color,gravedad,speed,imagen,life,salto){
-	this.x = 70;
+	this.x = canvas.width - 10;
 	this.y = 20;
 	this.w = 10;
 	this.h = 10;
 	this.color = 'red';
-	this.gravedad = 0;
+	this.gravedad = 1;
 	this.speed = 1;
 	this.imagen = 'URL';
 	this.life = 100;
 	this.salto = 30;
+	this.intento = 1;
+	this.damage = (canvas.width * 0.2);
 	}
 }
 
 const miPersonaje = new personaje();
+const miVida = new vida();
 const miEnemigo = new foe();
 
 const getCharacter = () => ({
@@ -159,18 +191,36 @@ const getCharacter = () => ({
 	imagen:miPersonaje.imagen,
 	g:miPersonaje.gravedad,
 	jump:miPersonaje.salto,
+	intento:miPersonaje.intento,
+	hit:miPersonaje.inmunidad,
 	
 	aparece(){
-		if(this.jump > 0){
-			this.g = 0;
-			this.y--;
-			this.jump--;
-		}else{
-			this.g = 1;
+		if(gameOver == 0){
+			if(this.life > 0){
+				//salto por gravedad
+				if(this.jump > 0){
+					this.g = 0;
+					this.y--;
+					this.jump--;
+					tu.intento = 0;
+				}else{
+					this.g = 1;
+					if(this.y <= canvas.height){
+						tu.intento = 1;
+					}
+				}
+				
+				//gravedad
+				if((this.y+this.h) < canvas.height){
+					this.y += this.g;
+				}
+				lugar.strokeStyle = this.color;
+				lugar.strokeRect(this.x,this.y,this.w,this.h);
+			}else{
+				confirm('GAME OVER!');
+				gameOver = 1;
+			}
 		}
-		
-		lugar.strokeStyle = this.color;
-		lugar.strokeRect(this.x,this.y,this.w,this.h);
 }
 
 });
@@ -184,16 +234,56 @@ const getEnemigo = () => ({
 	speed:miEnemigo.speed,
 	imagen:miEnemigo.imagen,
 	g:miEnemigo.gravedad,
+	damage:miEnemigo.damage,
 	
 	aparece(){
-		lugar.strokeStyle = this.color;
-		lugar.strokeRect(this.x,this.y,this.w,this.h);
+	
+		//gravedad
+		if((this.y+this.h) < canvas.height){
+			this.y += this.g;
+		}
+		
+		//mecanismo de ataque
+		if(this.life > 0 && this.x > 0){
+			this.x -= this.speed;
+			if(rango <= segundos){
+				tu.hit = 1;
+			}
+			if(this.x <= (tu.w+tu.x) && (this.x+this.w) >= tu.x && (tu.y+tu.h)>(canvas.height-this.h) && tu.life > 0 && tu.hit == 1){
+				
+				tu.life = tu.life - enemigo.damage;
+				tu.hit = 0;
+				rango = segundos + 4;
+			}
+			
+			lugar.strokeStyle = this.color;
+			lugar.strokeRect(this.x,this.y,this.w,this.h);
+		}else{
+			enemigo.x = canvas.width;
+			console.log(canvas.height-this.h);
+			console.log(tu.h + tu.y);
+		}
 	}
 
 });
 
+const getVida = () => ({
+	life:tu.life,
+	h:20,
+	x:0,
+	y:0,
+	color:'green',
+	
+	aparece(){
+		lugar.fillStyle = this.color;
+		lugar.fillRect(this.x,this.y,this.life,this.h);
+	}
+});
+
 var tu = getCharacter();
 var enemigo = getEnemigo();
+var enemigo2 = getEnemigo();
+var bar = getVida();
 
 console.log('TU: '+ tu);
 console.log('ENEMIGO: '+ enemigo);
@@ -202,17 +292,18 @@ console.log('ENEMIGO: '+ enemigo);
 const update = () => {
 lugar.clearRect(0,0,canvas.width,canvas.height);
 
-//gravedad
-if((tu.y+tu.h) < canvas.height){
-	tu.y += tu.g;
+bar.life = tu.life;
+if(bar.life < (canvas.width / 4)){
+	bar.color = 'red';
+}else if(bar.life < (canvas.width / 2)){
+	bar.color = 'orange';
 }
 
-
 tu.aparece();
+bar.aparece();
+
 enemigo.aparece();
 
-console.log('TU: ');
-console.log(tu);
 
 requestAnimationFrame(update);
 }
